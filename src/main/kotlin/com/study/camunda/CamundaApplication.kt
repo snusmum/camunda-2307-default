7d3448d484dcef8bbd5051ce9e2afba9214c210c
essm <nikolaev.s.g@mail.ru>
2022-04-09 13:30:32 +1000

    h2 version

---
+++ src/main/kotlin/com/study/camunda/CamundaApplication.kt
@@ -0,0 +1,11 @@
+package com.study.camunda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamundaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CamundaApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/camunda/CamundaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.camunda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamundaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
